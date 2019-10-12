15e3339151727c48786dc28956206653f61e43f4
Clément Hélardot <clement.helardot@gmail.com>
2019-10-12 15:56:27 +0200

    Cleaned anagram

--- anagrams/index.js
+++ anagrams/index.js
@@ -7,38 +7,36 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
+function cleanInput(str){
+    let regex = /[^\w]/g;
+    return str.replace(regex, "").toLowerCase().split('').sort();
+}
+
+function toMap(str){
+    let map = {};
+    for (const char of str) {
+        if (map[char] == undefined) {
+            map[char] = 1
+        } else {
+            map[char] += 1
+        }
+    }
+    return map
+}
+
 
 function anagrams(stringA, stringB) {
     let regex = /[^\w]/g;
-    let cleanA = stringA.replace(regex, "").toLowerCase().split('').sort();
-    let cleanB = stringB.replace(regex, "").toLowerCase().split('').sort();
-    
-    if (cleanA.length !== cleanB.length){
+    let cleanA = cleanInput(stringA);
+    let cleanB = cleanInput(stringB);
+
+    if (cleanA.length !== cleanB.length) {
         return false;
     } else {
-        let mapA = {};
-        let mapB = {};
+        let mapA = toMap(cleanA);
+        let mapB = toMap(cleanB);
 
-        for (const char of cleanA) {
-            if(mapA[char] == undefined){
-                mapA[char] = 1
-            } else {
-                mapA[char] += 1
-            }
-        }
-        for (const char of cleanB) {
-            if (mapB[char] == undefined) {
-                mapB[char] = 1
-            } else {
-                mapB[char] += 1
-            }
-        }
-        if (JSON.stringify(mapA) === JSON.stringify(mapB)){
-            return true;
-        } else {
-            return false;
-        }
-        
+        return JSON.stringify(mapA) === JSON.stringify(mapB) ? true : false;
     }
 }
 
