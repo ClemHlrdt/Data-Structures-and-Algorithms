2d520a97f8a63d14f6e30f6bc17899b8d5e96d3f
Clément Hélardot <clement.helardot@gmail.com>
2019-10-12 13:18:08 +0200

    maxchar done

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let max = 0;
+    let maxChar = '';
+    let charMap = {};
+    for (const char of str) {
+        charMap[char] === undefined ? charMap[char] = 1 : charMap[char] +=1;
+    }
+    
+    for (let char in charMap){
+        if(charMap[char] > max){
+            max= charMap[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
