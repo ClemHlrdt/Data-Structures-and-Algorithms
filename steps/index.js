cf9c7cbeacf4a9d30d50f6d8cc87f0d4ed94f441
Clément Hélardot <clement.helardot@gmail.com>
2019-10-12 17:44:59 +0200

    recursive steps solution added

--- steps/index.js
+++ steps/index.js
@@ -17,12 +17,45 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-    let steps = Array(n).fill(" ");
-    for (let i = 0; i < n; i++) {
-        steps[i] = "#";
-        console.log(steps.join(""));
+function steps(n, row = 0, stair = ''){
+    if(n === row){
+        return;
     }
+
+    if(n === stair.length){
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if(stair.length <= row){
+        stair += '#';
+    } else {
+        stair += " ";
+    }
+    steps(n, row, stair);
 }
-steps(3);
+
+// function steps(n) {
+//     let steps = Array(n).fill(" ");
+//     for (let i = 0; i < n; i++) {
+//         steps[i] = "#";
+//         console.log(steps.join(""));
+//     }
+// }
+
+// function steps(n){
+//     for (let row = 0; row < n; row++) {
+//         let stair = "";
+//         for (let column = 0; column < n; column++) {
+//             if(column <= row){
+//                 stair += "#"
+//             } else {
+//                 stair += " "
+//             }
+//         }
+//         console.log(stair);
+//     }
+//     return steps;
+// }
+
 module.exports = steps;
